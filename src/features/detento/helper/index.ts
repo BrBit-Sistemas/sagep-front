ce69181788ae713fa6a7179ad2852b292f7c15cf
alanrezendeee <alanrezendeee@gmail.com>
2025-09-22 13:22:57 -0300

    fix: ajustado edição detento para mostrar o nome da mãe

--- src/features/detento/helper/index.ts
+++ src/features/detento/helper/index.ts
@@ -1,15 +1,14 @@
 import type { Detento, DetentoFichaCadastral } from '../types';
 import type { CreateDetentoSchema, CreateDetentoFichaCadastralSchema } from '../schemas';
 
-import { Regime, Escolaridade } from '../types';
-
 export const detentoToFormValues = (detento: Detento): CreateDetentoSchema => ({
   nome: detento?.nome ?? '',
+  mae: detento?.mae ?? '',
   prontuario: detento?.prontuario ?? '',
   cpf: detento?.cpf ?? '',
   data_nascimento: detento?.data_nascimento ?? '',
-  regime: detento?.regime ?? Regime.FECHADO,
-  escolaridade: detento?.escolaridade ?? Escolaridade.FUNDAMENTAL_I_INCOMPLETO,
+  regime: detento?.regime ?? '',
+  escolaridade: detento?.escolaridade ?? '',
   unidade_id: detento?.unidade_id ?? '',
 });
 
